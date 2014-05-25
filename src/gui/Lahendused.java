4597b4ed7430efb9f0034b077f91282f7179c3af
Joosep Järve <joosep.jarve@gmail.com>
2014-05-26 01:51:46 +0300

    kuni 7. lahendused, 4. ei tööta sest hulgad on veel imelikud

--- src/gui/Lahendused.java
+++ src/gui/Lahendused.java
@@ -1,60 +1,95 @@
 package gui;
 
-import java.util.ArrayList;
-
 import ast.AstNode;
-import ast.astCreator.ASTCreator;
-import evaluator.Evaluator;
 
 public class Lahendused {
-	Evaluator eval;
-	public void setEvaluator(Evaluator eval) {
-		this.eval = eval;
+	String input;
+	String output;
+	String tree;
+
+	public void setInput(String input) {
+		this.input = input;
 	}
-	public Boolean isSolved(int lvl){
-		String output = eval.getOutput();
-		if(lvl == 0){
-			return true;			
-		} if (lvl == 1) {
-			return true; //nime sisestamine, kuidas kontrollida?
-		} if (lvl == 2) {
-			ArrayList<String> vastused2 = new ArrayList<String>();
-			vastused2.add("10+10");
-			vastused2.add("10+10\n20");
-			vastused2.add("20\n10+10");
-			vastused2.add("20");
-			if (vastused2.contains(output)){
+
+	public void setOutput(String output) {
+		this.output = output;
+	}
+	
+	public void setTree(String tree) {
+		this.tree = tree;
+	}
+	public Boolean isSolved(int lvl) {
+		if (lvl == 0) {
+			return true;
+		}
+		if (lvl == 1) {
+			if (input.startsWith("lausu(")) {
 				return true;
 			}
-			
-		} if (lvl == 3) {
-			
-		} if (lvl == 4) {
-			
-		} if (lvl == 5) {
-			
-		} if (lvl == 6) {
-			
-		} if (lvl == 7) {
-			
-		} if (lvl == 8) {
-			
-		} if (lvl == 9) {
-			
-		} if (lvl == 10) {
-			
-		} if (lvl == 11) {
-			
-		} if (lvl == 12) {
-			
-		} if (lvl == 13) {
-			
-		} if (lvl == 14) {
-			
+		}
+		if (lvl == 2) {
+			if ((input.startsWith("lausu(10+10)") && input
+					.indexOf("lausu(\"10+10\")") > 0)
+					|| (input.startsWith("lausu(\"10+10\")") && input
+							.indexOf("lausu(10+10)") > 0)) {
+				return true;
+			}
+		}
+		if (lvl == 3) {
+			if (output.indexOf(".") > 0) {
+				if (Float.parseFloat(output) == 1.75
+						|| input.startsWith("lausu(1.75)")) {
+					return true;
+				}
+			}
+		}
+		if (lvl == 4) {
+			if (output.startsWith("(") && output.endsWith(")")) {
+				return true;
+			}
+
+		}
+		if (lvl == 5) {
+
+			if (output.contains("jah\njah\njah\nei\nei\nei")){
+				return true;
+			}
+		}
+		if (lvl == 6) {
+			if (tree.contains("Omistamine") && tree.contains("lausu\", [Muutuja")){
+					return true;
+			}
+		}
+		if (lvl == 7) {
+			if (input.contains("sisu=\"jätka\"\nlausu(sisu)")){
+				return true;
+			}
+
+		}
+		if (lvl == 8) {
+			if (tree.contains("Omistamine") && tree.contains("jäätis") && tree.contains("17") && tree. contains("Hulk(SõneLiteraal")){
+				return true;
+			}
+
+		}
+		if (lvl == 9) {
+
+		}
+		if (lvl == 10) {
+
+		}
+		if (lvl == 11) {
+
+		}
+		if (lvl == 12) {
+
+		}
+		if (lvl == 13) {
+
+		}
+		if (lvl == 14) {
+
 		}
 		return false;
 	}
-
-
-
 }
